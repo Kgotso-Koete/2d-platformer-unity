885b808c3046ed42310c623fa63b88f3e9f286b2
Kgotso Koete <kgkoete@gmail.com>
2022-10-06 21:46:19 +0100

    jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D theRB;
+    public float jumpForce;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x,jumpForce);
+        }
     }
 }     
