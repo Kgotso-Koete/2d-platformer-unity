c0d6ca5e0aa8e7d3ca33f3fad8b60d82128601d0
Kgotso Koete <kgkoete@gmail.com>
2022-10-09 22:40:42 +0100

    adding double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
@@ -10,26 +9,34 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
-
+    private bool canDoubleJump;
     // Start is called before the first frame update
     void Start()
     {
     }
-
     // Update is called once per frame
     void Update()
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position,.2f, whatIsGround);
-
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
         if (Input.GetButtonDown("Jump"))
         {
             if (isGrounded)
             {
-                theRB.velocity = new Vector2(theRB.velocity.x,jumpForce);
-
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
             }
-            
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
         }
     }
-}     
+}
