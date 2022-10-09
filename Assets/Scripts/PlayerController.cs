d4a2c886f198d6baf10997e7501297e4e6095f1b
Kgotso Koete <kgkoete@gmail.com>
2022-10-10 00:56:43 +0100

    controlling the animator and changing direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,19 +12,23 @@ public class PlayerController : MonoBehaviour
     private bool canDoubleJump;
     // Start is called before the first frame update
     private Animator anim;
+    private SpriteRenderer theSR;
     void Start()
     {
         anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void Update()
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+        // set double jump variable
         if (isGrounded)
         {
             canDoubleJump = true;
         }
+        // execute jump updates
         if (Input.GetButtonDown("Jump"))
         {
             if (isGrounded)
@@ -40,6 +44,15 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
+        // flip animations to the left / make sprite face left
+        if(theRB.velocity.x < 0){
+            theSR.flipX = true;
+        }
+        // flip back to face the right
+        else if(theRB.velocity.x > 0){
+            theSR.flipX = false;
+        }
+        // update animation variables
         anim.SetFloat("moveSpeed",Mathf.Abs(theRB.velocity.x));
         anim.SetBool("isGrounded", isGrounded);
     }
