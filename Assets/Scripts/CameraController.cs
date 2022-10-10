845952c42fef1df71859cdc003ce2c294b2693d1
Kgotso Koete <kgkoete@gmail.com>
2022-10-10 21:54:29 +0100

    adding parallax for depth

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CameraController : MonoBehaviour
 {
+    // for the camera to follow a sprite
     public Transform target;
+    // objects for parallax scrolling
+    public Transform farBackground, middleBackground;
+    private float lastXPos;
     // Start is called before the first frame update
     void Start()
     {
+        lastXPos = transform.position.x;
     }
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x,transform.position.y,transform.position.z);
+        // create positions for parallax scrolling
+        float amountToMoveX = transform.position.x - lastXPos;
+        farBackground.position += new Vector3(amountToMoveX,0f,0f);
+        middleBackground.position += new Vector3(amountToMoveX * .5f,0f,0f);
+        lastXPos = transform.position.x;
     }
 }
