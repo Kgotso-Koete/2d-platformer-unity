baab87b303b33dc3f71fde288d3b92ddb006071d
Kgotso Koete <kgkoete@gmail.com>
2022-10-10 22:21:13 +0100

    clamping camera vertically

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,8 +5,10 @@ public class CameraController : MonoBehaviour
 {
     // for the camera to follow a sprite
     public Transform target;
-    // objects for parallax scrolling
+    // variable for parallax scrolling
     public Transform farBackground, middleBackground;
+    // variable for vertical camera control
+    public float minHeight, maxHeight;
     private float lastXPos;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,11 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x,transform.position.y,transform.position.z);
+        //transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);
+        // update camera's y position within limits
+        //float clampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight);
+        //transform.position = new Vector3(transform.position.x, clampedY,transform.position.z);
+        transform.position = new Vector3(target.position.x,Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z); 
         // create positions for parallax scrolling
         float amountToMoveX = transform.position.x - lastXPos;
         farBackground.position += new Vector3(amountToMoveX,0f,0f);
