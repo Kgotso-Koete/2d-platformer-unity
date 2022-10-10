37a725ea3a508db0ea8388a6fad4c680f18afbb6
Kgotso Koete <kgkoete@gmail.com>
2022-10-10 22:40:46 +0100

    challenge - parallax vertically

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,24 +9,21 @@ public class CameraController : MonoBehaviour
     public Transform farBackground, middleBackground;
     // variable for vertical camera control
     public float minHeight, maxHeight;
-    private float lastXPos;
+    private Vector2 lastPos;
     // Start is called before the first frame update
     void Start()
     {
-        lastXPos = transform.position.x;
+        lastPos = transform.position;
     }
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);
         // update camera's y position within limits
-        //float clampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight);
-        //transform.position = new Vector3(transform.position.x, clampedY,transform.position.z);
-        transform.position = new Vector3(target.position.x,Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z); 
+        transform.position = new Vector3(target.position.x,Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
         // create positions for parallax scrolling
-        float amountToMoveX = transform.position.x - lastXPos;
-        farBackground.position += new Vector3(amountToMoveX,0f,0f);
-        middleBackground.position += new Vector3(amountToMoveX * .5f,0f,0f);
-        lastXPos = transform.position.x;
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
+        farBackground.position += new Vector3(amountToMove.x,amountToMove.y,0f);
+        middleBackground.position += new Vector3(amountToMove.x ,amountToMove.y,0f) * .5f;
+        lastPos = transform.position;
     }
 }
