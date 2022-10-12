a464ca7d4902143a3ba27d85e9e12299c4198386
Kgotso Koete <kgkoete@gmail.com>
2022-10-12 21:28:21 +0100

    detecting spikes hitting the player

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth,maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void DealDamage()
+    {
+        currentHealth -= 1;
+        if(currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
