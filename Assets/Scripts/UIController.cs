4bdb809bbd443df2efa5212b7386ccbe178c08fa
Kgotso Koete <kgkoete@gmail.com>
2022-10-16 14:39:02 +0100

    showing half hearts

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,7 +6,7 @@ public class UIController : MonoBehaviour
 {
     public static UIController instance;
     public Image heart1,heart2,heart3;
-    public Sprite heartFull, heartEmpty;
+    public Sprite heartFull, heartEmpty, heartHalf;
     private void Awake()
     {
         instance = this;
@@ -23,18 +23,33 @@ public class UIController : MonoBehaviour
     {
         switch(PlayerHealthController.instance.currentHealth)
         {
-            case 3:
+            case 6:
                 heart1.sprite = heartFull;
                 heart2.sprite = heartFull;
                 heart3.sprite = heartFull;
                 break;
-            case 2:
+            case 5:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartHalf;
+                break;
+            case 4:
                 heart1.sprite = heartFull;
                 heart2.sprite = heartFull;
                 heart3.sprite = heartEmpty;
                 break;
-            case 1:
+            case 3:
                 heart1.sprite = heartFull;
+                heart2.sprite = heartHalf;
+                heart3.sprite = heartEmpty;
+                break;
+            case 2:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+            case 1:
+                heart1.sprite = heartHalf;
                 heart2.sprite = heartEmpty;
                 heart3.sprite = heartEmpty;
                 break;
