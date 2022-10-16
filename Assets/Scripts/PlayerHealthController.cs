f9be41770840d2b6c57d3f2c437cb9d62643f2a4
Kgotso Koete <kgkoete@gmail.com>
2022-10-16 21:52:18 +0100

    adding invincibility

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -5,6 +5,8 @@ public class PlayerHealthController : MonoBehaviour
 {
     public static PlayerHealthController instance;
     public int currentHealth,maxHealth;
+    public float invincibleLength;
+    private float invincibleCounter;
     // initialize singleton to instantiated object
     private void Awake()
     {
@@ -18,18 +20,27 @@ public class PlayerHealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(invincibleCounter > 0){
+            // decrease by 1 every second by deducting time to between frames each update (60 updates per second for 60 frames per second)
+            invincibleCounter -= Time.deltaTime;
+        }
     }
     public void DealDamage()
     {
-        // decrease health
-        currentHealth -= 1;
-        // check if health is depleted
-        if(currentHealth <= 0)
-        {
-            currentHealth = 0;
-            gameObject.SetActive(false);
+        if(invincibleCounter <= 0){
+            // decrease health
+            currentHealth -= 1;
+            // check if health is depleted
+            if(currentHealth <= 0)
+            {
+                currentHealth = 0;
+                gameObject.SetActive(false);
+            }
+            else{
+                invincibleCounter = invincibleLength;
+            }
+            // update health UI
+            UIController.instance.updateHealthDisplay();
         }
-        // update health UI
-        UIController.instance.updateHealthDisplay();
     }
 }
