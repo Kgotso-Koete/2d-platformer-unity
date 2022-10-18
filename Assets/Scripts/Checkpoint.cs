ee93bb946d96321a8be24a427e6e607bb9e89cfe
Kgotso Koete <kgkoete@gmail.com>
2022-10-18 21:18:57 +0100

    making checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer theSR;
+    public Sprite cpOn,cpOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            theSR.sprite = cpOn;
+        }
+    }
+}
