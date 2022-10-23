bc1494c6df225c3a6489d25a97911e150b20c382
Kgotso Koete <kgkoete@gmail.com>
2022-10-23 12:40:18 +0100

    moving the frog

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform leftPoint,rightPoint;
+    private bool movingRight;
+    private Rigidbody2D theRB;
+    public SpriteRenderer theSR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+        leftPoint.parent = null;
+        rightPoint.parent = null;
+        movingRight = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(movingRight)
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+            theSR.flipX = true;
+            if(transform.position.x > rightPoint.position.x)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+            theSR.flipX = false;            
+            if(transform.position.x < leftPoint.position.x)
+            {
+                movingRight = true;
+            }
+        }
+    }
+}
