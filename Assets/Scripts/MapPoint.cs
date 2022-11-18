4f572a3f0ec040b0376414bb7462a85a9d89dbc6
Kgotso Koete <kgkoete@gmail.com>
2022-11-18 17:12:27 +0000

    showing badges

--- Assets/Scripts/MapPoint.cs
+++ Assets/Scripts/MapPoint.cs
@@ -8,6 +8,7 @@ public class MapPoint : MonoBehaviour
     public string levelToLoad, levelToCheck,levelName;
     public int gemsCollected, totalGems;
     public float bestTime, targetTime;
+    public GameObject gemBadge, timeBadge;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,14 @@ public class MapPoint : MonoBehaviour
             if(PlayerPrefs.HasKey(levelToLoad + "_time"))
             {
                 bestTime = PlayerPrefs.GetFloat(levelToLoad +"_time");
+            }
+            if(gemsCollected >= totalGems)
+            {
+                gemBadge.SetActive(true);
+            }
+            if(bestTime <= targetTime && bestTime != 0)
+            {
+                timeBadge.SetActive(true);
             }
             isLocked = true;
             // check if a level should be unlocked
