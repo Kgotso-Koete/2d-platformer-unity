a2af69433af52a4ea3c98870e16f164263f5bdb8
Kgotso Koete <kgkoete@gmail.com>
2022-12-27 11:48:37 +0000

    setting up the boss basics

---
+++ Assets/Scripts/BossTankController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BossTankController : MonoBehaviour
+{
+    public Transform theBoss;
+    public Animator anim;
+    [Header("Movement")]
+    public float moveSpeed;
+    public Transform leftPoint, rightPoint;
+    private bool moveRight;
+    [Header("Shooting")]
+    public GameObject bullet;
+    public Transform firePoint;
+    public float timeBetweenShots;
+    private float shotCounter;
+    [Header("Hurt")]
+    public float hurtTime;
+    private float hurtCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
