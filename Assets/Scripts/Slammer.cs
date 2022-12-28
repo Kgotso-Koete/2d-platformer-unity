eb336a7b26a5d604e1ced641046ff653442826db
Kgotso Koete <kgkoete@gmail.com>
2022-12-28 18:08:59 +0000

    added slammers

--- Assets/Scripts/Slammer.cs
+++ Assets/Scripts/Slammer.cs
@@ -1,40 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Slammer : MonoBehaviour
 {
     public Transform theSlammer, slammerTarget;
     private Vector3 startPoint;
-
     public float slamSpeed, waitAfterSlam, resetSpeed;
     private float waitCounter;
     private bool slamming, resetting;
-
     // Start is called before the first frame update
     void Start()
     {
         startPoint = theSlammer.position;
     }
-
     // Update is called once per frame
     void Update()
     {
         if (!slamming && !resetting)
         {
-            if (Vector3.Distance(slammerTarget.position, PlayerController.instance.transform.position) < 2f)
+            float slamDistance = Vector3.Distance(slammerTarget.position, PlayerController.instance.transform.position);
+            if (slamDistance < 2f)
             {
                 slamming = true;
                 waitCounter = waitAfterSlam;
             }
         }
-
         if (slamming)
         {
             theSlammer.position = Vector3.MoveTowards(theSlammer.position, slammerTarget.position, slamSpeed * Time.deltaTime);
-
-
-
             if (theSlammer.position == slammerTarget.position)
             {
                 waitCounter -= Time.deltaTime;
@@ -43,14 +36,11 @@ public class Slammer : MonoBehaviour
                     slamming = false;
                     resetting = true;
                 }
-
             }
         }
-
         if (resetting)
         {
             theSlammer.position = Vector3.MoveTowards(theSlammer.position, startPoint, resetSpeed * Time.deltaTime);
-
             if (theSlammer.position == startPoint)
             {
                 resetting = false;
